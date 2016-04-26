fcea41c49ffdf943a8591c045f7a2d1babed40f8
mikiyas <mikiyast@icloud.com>
2016-04-26 14:24:10 -0400

    added detailed view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,5 +7,9 @@ from .models import Post
 
 # first view - the list of posts ordered y publishing date 
 def post_list(request):
-    posts = Post.objects.filter(published_Date__lte=timezone.now()).order_by('published_Date')
+    posts = Post.objects.filter(published_Date__lte=timezone.now()).order_by('-published_Date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+    
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    return render(request, 'blog/post_detail.html',{'post':post})
