803ef1f76a79f547c1784540577e672e9f2b2874
mikiyas <mikiyast@icloud.com>
2016-04-28 11:45:42 -0400

    finished

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -17,7 +18,8 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk = pk)
     return render(request, 'blog/post_detail.html',{'post':post})
   
-# third view - post new entry   
+# third view - post new entry
+@login_required   
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -32,6 +34,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form':form})
     
 # fourth view - editing existing posts 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -45,3 +48,29 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form}) 
+    
+
+# fifth view - delete post 
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    post.delete()
+    return redirect('blog.views.post_list')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
